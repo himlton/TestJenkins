8dacc79a2be4b69d9da228756639b794b45c1417
himlton <hiteshmangal@gmail.com>
2020-06-04 21:45:00 -0500

    Project Created

---
+++ src/test/java/TestJenkins/TestJenkins/Test.java
@@ -0,0 +1,12 @@
+package TestJenkins.TestJenkins;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Jai Shri Ram");
+
+	}
+
+}
